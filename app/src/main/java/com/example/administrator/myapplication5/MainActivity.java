1b60ff21f8ca7f0a6e230bcfbe97d34f78693a1d
sailor1024 <1663852293@qq.com>
2017-09-11 21:03:00 +0800

    2017.09.11  21:02

---
+++ app/src/main/java/com/example/administrator/myapplication5/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.myapplication5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("20170911 20:59");
+
+    }
+}
